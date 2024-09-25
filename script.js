d620242e301e4890330c14115bd855aea0f14a54
JayashreeChelladurai <shree.rcj@gmail.com>
2024-09-25 09:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// // function add(a,b)
+// // {
+// //     return (a+b);
+// // }
+// // console.log(add(8,900));
+//  add=(a,b,c,d)=>
+//     {
+//          s1=a+b;
+//          s2= c+d;
+//          tot= s1+s2;
+//         return tot;
+//     }
+
+// console.log(add(80,900,20,20));
+
+// function check()
+// {
+//     const m="ab"
+//     if(true)
+//     {
+//       const  m="ex"
+//         console.log(m);
+//     }
+//     console.log(m);
+
+// }
+// check()
+
+// function examplefunction(){
+//     if (true)
+//     {
+//     let x=10;const y=20;
+//      console.log(x);console.log(y)}
+     
+     
+//      console.log(x);
+// console.log(y);
+//     }
+// //     examplefunction();
+
+// const num=[1,2,3,4,5]
+// const [f, s,t] = num
+// console.log(f,s,t);
+// Spread operator
+
+// const orgarr={a:1, b:2};
+// const coparr={...orgarr}
+// const merge={...orgarr, ...coparr}
+// console.log(orgarr===coparr)
+
+const word = 'EDUNET'
+const letter = [...word]
+console.log(letter)
